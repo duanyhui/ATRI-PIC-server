f9c5287b395e41c9f4540e0bd9b21761b8f46f33
duanyhui <91049287+duanyhui@users.noreply.github.com>
2022-08-23 15:26:19 +0800

    首次提交

---
+++ src/main/java/duan/atripic/AtriPicApplication.java
@@ -0,0 +1,13 @@
+package duan.atripic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtriPicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtriPicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/duan/atripic/AtriPicApplicationTests.java
@@ -0,0 +1,13 @@
+package duan.atripic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtriPicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
